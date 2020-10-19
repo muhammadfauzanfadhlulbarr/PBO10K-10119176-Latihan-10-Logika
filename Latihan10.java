f0e9fa780def0443c532344780612392b34f7241
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-10-19 10:54:53 +0700

    commit

---
+++ Latihan10.java
@@ -0,0 +1,8 @@
+public class Latihan10 {
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
