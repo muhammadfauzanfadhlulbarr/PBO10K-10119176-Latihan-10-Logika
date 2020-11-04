6e6a61d3b0686602d86b965b62d9f562da3ec9a9
Muhammad Fauzan Fadhlulbarr <52595452+muhammadfauzanfadhlulbarr@users.noreply.github.com>
2020-11-04 15:10:39 +0700

    Update Latihan10.java

--- Latihan10.java
+++ Latihan10.java
@@ -1,3 +1,10 @@
+/**
+ * Latihan10
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program  : Logika
+ */
 public class Latihan10 {
     public static void main(String[] args) {
         int umur = 19;
